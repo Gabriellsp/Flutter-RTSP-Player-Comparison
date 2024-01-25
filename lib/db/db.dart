7c78ead94b9f69e6c9757fcd0dafbf5684730df5
Gabriel Pereira <gabriel.pereira@seventh.com.br>
2024-01-25 17:01:37 -0300

    Removendo cache do db

--- lib/db/db.dart
+++ lib/db/db.dart
@@ -1,8 +1,5 @@
 class Db {
-  static String rtspUrl =
-      'rtsp://admin:seventh2019@187.49.236.67:43851/ISAPI/streaming/channels/101';
-  static String rtspNimbleUrl =
-      'rtsp://cloudcam01-development.dguardcloud.com.br:9554/live/1Z9BddxtB.stream';
-  static String hlsUrl =
-      'https://cloudcam01-development.dguardcloud.com.br/live/1Z9BddxtB.stream/playlist.m3u8';
+  static String rtspUrl = '';
+  static String rtspNimbleUrl = '';
+  static String hlsUrl = '';
 }
